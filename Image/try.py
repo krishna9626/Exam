648d4c17a49b85d2cd591cae3cfb64958da16185
krishna9626 <krishna@svayamlabs.com>
2018-06-22 15:27:52 +0530

    Initial Commit

---
+++ Image/try.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import numpy as np
+
+i=Image.open('images/dot1.png')
+num1=np.asarray(i)
+
+j=Image.open('images/dot3.png')
+num2=np.asarray(j)
+
+first=str(num1)
+second=str(num2)
+
+if first == second:
+    print ("exists")
+else:
+    print ("Not exists")
+
